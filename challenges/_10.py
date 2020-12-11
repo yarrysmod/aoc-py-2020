103925b0de6468958b0f0f779008d1febc7431ec
yarrysmod <webuser@github.com>
2020-12-11 11:35:17 +0100

    [dev]: added solution for 10 part 2

--- challenges/_10.py
+++ challenges/_10.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from common import exec_func, get_input_lines
 
 FILE_NUM = __file__[-5:-3]
@@ -38,53 +36,27 @@ def solve1():
     return diff_map[1] * diff_map[3]
 
 
-def get_removable_adapters(root_adapter, adapters):
-    removable_adapters = []
-
-    for index, adapter in enumerate(adapters[:-1]):
-        next_adapter = adapters[index]
-        following_adapter = adapters[index + 1]
-        diff_to_next = next_adapter - root_adapter
-        diff_to_following = following_adapter - root_adapter
-
-        if diff_to_next > MAX_DIFF or diff_to_following > MAX_DIFF:
-            break
-
-        removable_adapters.append(adapter)
-
-    return removable_adapters
-
-
-def get_arrangement_count(removable_adapters, adapters: List[int]):
-    arrangements_count = len(removable_adapters)
-
-    for adapter in removable_adapters:
-        reduced_adapters = list(adapters)
-        reduced_adapters.remove(adapter)
-        removable_adapters = get_removable_adapters(reduced_adapters[0], reduced_adapters[1:])
-
-        if len(removable_adapters) > 0:
-            arrangements_count += get_arrangement_count(removable_adapters, reduced_adapters)
-
-    return arrangements_count
-
-
-def solve2(): # TODO
+def solve2():
     adapters = [0] + sorted(map(int, get_input_lines(FILE_NUM)))
-    adapters_til_third_to_last = adapters[:-2]
-    arrangements_count = 1
+    possible_edges = [1] + ([0] * (max(adapters)))
+    edges_count = len(possible_edges)
 
-    for index, adapter in enumerate(adapters_til_third_to_last):
-        next_adapter_index = index + 1
-        adapters_after_current = adapters[next_adapter_index:-1]
-        removable_adapters = get_removable_adapters(adapter, adapters_after_current)
-        nested_arrangements = get_arrangement_count(removable_adapters, adapters_after_current)
+    for edge_index in range(edges_count):
+        # skip the edge if it's not in the adapters
+        if edge_index not in adapters:
+            continue
 
-        arrangements_count += nested_arrangements
+        reachable_edges = []
 
-    return arrangements_count
+        for previous_index in range(edge_index - 3, edge_index):
+            if previous_index >= 0:
+                reachable_edges.append(possible_edges[previous_index])
+
+        possible_edges[edge_index] += sum(reachable_edges)
+
+    return possible_edges[-1]
 
 
 if __name__ == '__main__':
     exec_func(solve1, 2760)
-    exec_func(solve2)
+    exec_func(solve2, 13816758796288)
