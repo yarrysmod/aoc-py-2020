b2853cb31775e4081d7c3a009977a4e469f97846
yarrysmod <web@github.com>
2021-12-08 15:35:59 +0100

    [dev]: 2021 - adding solution for 06 part 2

--- 2021/challenges/_06.py
+++ 2021/challenges/_06.py
@@ -24,8 +24,35 @@ def solve1(is_sample=False):
 
 def solve2(is_sample=False):
     lines = list(get_input_lines(FILE_NUM, SAMPLES_FOLDER if is_sample else None, HOME_DIR))
+    fishes = [int(x) for x in lines[0].split(',')]
+    fish_counts = get_fish_counts(fishes)
+    pending_new_fish = 0
 
-    pass
+    for step in range(256):
+        for count_index in range(len(fish_counts)):
+            if count_index == 0:
+                pending_new_fish = fish_counts[count_index]
+            else:
+                fish_counts[count_index - 1] = fish_counts[count_index]
+
+            fish_counts[count_index] = 0
+
+        # moving the old fish and new fish to their new positions
+        if pending_new_fish:
+            fish_counts[6] += pending_new_fish
+            fish_counts[8] += pending_new_fish
+            pending_new_fish = 0
+
+    return sum(fish_counts)
+
+
+def get_fish_counts(fishes):
+    fish_list = [0] * 9
+
+    for fish in fishes:
+        fish_list[fish] += 1
+
+    return fish_list
 
 
 if __name__ == '__main__':
